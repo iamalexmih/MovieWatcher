29f325d8d0d3c39058dc8c47cc77a42eba460a79
Alex <alexmihgel@gmail.com>
2023-04-02 18:33:06 +0300

    Initial Commit

---
+++ ch 3 MovieWatcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ch 3 MovieWatcher
+//
+//  Created by Алексей Попроцкий on 02.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
